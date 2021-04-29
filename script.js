07cd09241bdd0bc0806012ec6749e3482c665a50
Kai Fung Man <kaifungman@Gogs-laptop.fritz.box>
2021-04-29 17:54:45 +0100

    Script.js edited

--- script.js
+++ script.js
@@ -2,30 +2,16 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-function generateTarget(){
-  let number = Math.random() *10
-  number = Math.floor(number)
-  return number
-}
-
-function alert(input){
-  if(input > 9){
-    console.log('Number out of range')
-  }
-}
+// Generate random numbers
+const generateTarget = () => Math.floor(Math.random() *10)
 
 //Get absolute distance between numbers
-function getAbsoluteDistance(input, target){
-  let absDist = target - input
-  if(Math.sign(absDist)=== -1){
-    absDist = -absDist
-  }
-  return absDist
-}
+const getAbsoluteDistance = (input, target) => Math.abs(target - input)
 
-function compareGuesses(human, computer, target){
-  alert(human);
+const compareGuesses = (human, computer, target) => {
+  if(human > 9 || human < 0){
+    alert('Number out of range')
+  }
   let humanGuess = getAbsoluteDistance(human,target)
   let compGuess = getAbsoluteDistance(computer,target)
   if(compGuess > humanGuess){
@@ -35,7 +21,7 @@ function compareGuesses(human, computer, target){
   }
 }
 
-function updateScore(winner){
+const updateScore = winner => {
   if(winner === 'human'){
     humanScore++;
   } else if (winner === 'computer'){
@@ -43,8 +29,7 @@ function updateScore(winner){
   }
 }
 
-function advanceRound(){
-  currentRoundNumber++;
-}
+const advanceRound = () => currentRoundNumber++;
+
 
 
